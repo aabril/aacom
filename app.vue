558e6984b4b4a1f2e865134c80361c4e0c45c766
John Galt <galt@fedora.com>
2023-03-03 03:12:26 +0100

    all sorted

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
 <template>
    <NuxtPage class="prose" />
 </template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto:900');
+@import '~/assets/normalize.scss';
+@import '~/assets/colors.scss';
+@import '~/assets/skeleton/skeleton.scss';
+
+</style>
